9997b0bef091f481c212a0a80b6e733cd2e4c2be
yyalexyy <44172655+ajuan3@users.noreply.github.com>
2021-07-18 20:57:12 -0400

    import ngModule for two way binding

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -10,7 +11,8 @@ import { ProductListComponent } from './products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent] // starting component for our app
 })
