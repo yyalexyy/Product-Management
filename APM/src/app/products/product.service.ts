9bae47ad3855e9f4920ce2e6c3e1b4a561905d88
yyalexyy <44172655+ajuan3@users.noreply.github.com>
2021-07-19 01:04:17 -0400

    implement http get request for product service

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,43 +1,37 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from 'rxjs/operators';
 import { IProduct } from "./product";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    private productUrl = 'api/products/products.json';
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            }, 
-            {
-                "productId": 5,
-                "productName": "Hammer",
-                "productCode": "Tbx-0048",
-                "releaseDate": "May 21, 2021",
-                "description": "Curved claw steel hammer",
-                "price": 8.99,
-                "starRating": 4.8,
-                "imageUrl": "assets/images/hammer.png"
-            },
-            {
-                "productId": 10,
-                "productName": "Video Game Controller",
-                "productCode": "GMG-0042",
-                "releaseDate": "October 15, 2020",
-                "description": "Standard two-button video game controller",
-                "price": 35.95,
-                "starRating": 4.6,
-                "imageUrl": "assets/images/xbox-controller.png"
-              }
-        ];
+    constructor(private http: HttpClient) {}
+
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).pipe(
+            tap(data => console.log('All', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+          // A client-side or network error occurred. Handle it accordingly.
+          errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
     }
 }
